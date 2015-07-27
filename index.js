8b2be381f0739ee66bee2a148dc6d1f92e1b5f03
Jennifer Gebske <jennifer@gebske.de>
2015-07-27 17:11:48 +0200

    table and map with dummy data

--- index.js
+++ index.js
@@ -8,4 +8,47 @@ app.controller('DemoController', [ '$scope', function($scope) {
             zoom: 5
         }
     });
+
+    // dummy data
+    $scope.objects = [
+          {
+            "id": "1",
+            "standort": "Bild",
+            "beschreibung": "Aabachstrasse 16",
+            "wochenpreis": "48",
+            "format": "F4M",
+            "ort": "Zug-Herti",
+            "latitude": 47.17717693671103,
+            "longitude": 8.506978072183173
+        },
+        {
+            "id": "2",
+            "standort": "Bild",
+            "beschreibung": "Alte Landstrasse 192 - neben ZVB Abri",
+            "wochenpreis": "240",
+            "format": "F12",
+            "ort": "Unterägeri",
+            "latitude": 47.1410163,
+            "longitude": 8.573110799999995
+        },
+        {
+            "id": "3",
+            "standort": "Bild",
+            "beschreibung": "Florissant, Routede 52",
+            "wochenpreis": "162",
+            "format": "F12",
+            "ort": "Genève-Champel",
+            "latitude": 46.1927354,
+            "longitude": 6.162045300000045
+        },
+        {
+            "id": "4",
+            "standort": "Bild",
+            "beschreibung": "Fontaine, Chemin de la 5",
+            "wochenpreis": "385",
+            "format": "F12L",
+            "ort": "Nyon",
+            "latitude": 46.3878733,
+            "longitude": 6.220048799999972
+        }];
 }]);
